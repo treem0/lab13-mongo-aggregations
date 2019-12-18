bb8f29e980b985310942539d9bf179341b494c3e
Travis Molter <travismolter@gmail.com>
2019-12-18 13:34:14 -0800

    file structure setup, model created, seed data function, working on routes. Have the post and get by ID route wrote and tested

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# lab13-mongo-aggregations
+LAB: Mongo Aggregations
+Create an app with all CRUD routes for a model. Use the aggregation you explored yesterday to create additional routes.
+
+Before you begin
+Refer to Getting Started in the lab submission instructions for complete setup, configuration, deployment, and submission instructions.
+
+Requirements
+Write a seed data script
+Create a script that can seed your database. Use your script to seed your database before your tests.
+
+Write CRUD routes for a model
+Create a model and all CRUD routes for the model
+
+Create static methods for aggregations
+Write static methods on your model to run your aggregations
+
+Create routes to get your aggregations
+Assignment Submission Instructions
+Refer to the the lab submission instructions for the complete lab submission process and expectations
---
+++ __tests__/app.test.js
@@ -0,0 +1,78 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Film = require('../lib/models/Film');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  let film;
+  beforeEach(async() =>{
+    film = await Film.create({
+      Year: 1990,
+      Title: 'Treemo Money',
+      Subject: 'Action',
+      Actor: 'Travis Molter',
+      Actress: 'Emma Watson',
+      Director: 'Travis Molter',
+      Popularity: 1
+    });
+  });
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can create a new film', () => {
+    return request(app)
+      .post('/api/v1/films')
+      .send({
+        Year: 1990,
+        Title: 'Treemo Money',
+        Subject: 'Action',
+        Actor: 'Travis Molter',
+        Actress: 'Emma Watson',
+        Director: 'Travis Molter',
+        Popularity: 1
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          Year: 1990,
+          Title: 'Treemo Money',
+          Subject: 'Action',
+          Actor: 'Travis Molter',
+          Actress: 'Emma Watson',
+          Director: 'Travis Molter',
+          Popularity: 1,
+          __v: 0
+        });
+      });
+  });
+
+  it('gets a film by id', () =>{
+    return request(app)
+      .get(`/api/v1/films/${film.id}`)
+      .then(res =>{
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          Year: 1990,
+          Title: 'Treemo Money',
+          Subject: 'Action',
+          Actor: 'Travis Molter',
+          Actress: 'Emma Watson',
+          Director: 'Travis Molter',
+          Popularity: 1,
+          __v: 0
+        });
+      });
+  });
+});
